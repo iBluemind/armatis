81112afb181e88a87b3399b8f7a1f0462ab382cc
Manjong Han <aksmj8855@gmail.com>
2016-12-05 12:18:11 +0900

    Constants: Fix the default user-agent correctly

--- kparcel/constants.py
+++ kparcel/constants.py
@@ -1,4 +1,4 @@
-PARSER_REQUEST_HEADER_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:2.0) Gecko/20100101 Firefox/4.0'
+PARSER_REQUEST_HEADER_USER_AGENT = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'
 PARSER_RESULT_PARCEL = 'parcel'
 PARSER_RESULT_TRACKS = 'tracks'
 
